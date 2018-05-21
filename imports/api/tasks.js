510c1faa4e22480afde659c363438d1dbc88171a
mmunozpatino <mercedesmp1305@gmail.com>
2018-05-21 09:27:21 -0300

    paso 10.5 completo

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -7,6 +7,12 @@ import Task from '../ui/Task';
 
 export const Tasks = new Mongo.Collection('tasks');
 
+if(Meteor.isServer){
+    //esto se ejecuta en el server unicamente
+    Meteor.publish('tasks', function tasksPublication(){
+        return Tasks.find();
+    });
+}
 Meteor.methods({
     'tasks.insert'(text){
         check(text, String);
@@ -34,4 +40,16 @@ Meteor.methods({
 
         Tasks.update(taskId, { $set: {checked: setChecked}});
     },
+    'tasks.setPrivate'(taskId, setToPrivate){
+        check(taskId, String);
+        check(setToPrivate, Boolean);
+
+        const task = Tasks.findOne(taskId);
+
+        if(task.owner != this.userId){
+            throw new Meteor.Error('not-authorized');
+        }
+
+        Tasks.update(taskId,{ $set: {private: setToPrivate}});
+    }
 });
--- imports/ui/App.js
+++ imports/ui/App.js
@@ -30,9 +30,18 @@ class App extends Component {
         if (this.state.hideCompleted) {
             filteredTasks = filteredTasks.filter(task => !task.checked);
         }
-        return filteredTasks.map((task) => (
-            <Task key={task._id} task={task} />
-        ));
+        return filteredTasks.map((task) => {
+            const currentUserId = this.props.currentUser && this.props.currentUser._id;
+            const showPrivateButton = task.owner === currentUserId;
+            
+            return (
+                <Task        
+                  key={task._id}        
+                  task={task}        
+                  showPrivateButton={showPrivateButton}        
+                />        
+              );
+        });
     }
     handleSubmit(event) {
         event.preventDefault();
@@ -80,6 +89,7 @@ class App extends Component {
 }
 
 export default withTracker(() => {
+    Meteor.subscribe('tasks');
     return {
         tasks: Tasks.find({}, { sort: { createAt: -1 } }).fetch(),
         incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
